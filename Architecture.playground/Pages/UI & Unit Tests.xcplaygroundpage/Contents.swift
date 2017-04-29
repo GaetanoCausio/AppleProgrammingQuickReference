adf3ae9474b31ab326bd846f9ea66157cbbab045
Gaetano Causio <gaetano.causio@gmail.com>
2017-04-29 15:45:53 +0200

    Updated info over unit testing

--- Architecture.playground/Pages/UI & Unit Tests.xcplaygroundpage/Contents.swift
+++ Architecture.playground/Pages/UI & Unit Tests.xcplaygroundpage/Contents.swift
@@ -5,15 +5,20 @@
  ## Add a Unit Test
  
  1) In Xcode add a new target of type "... Unit Testing Bundle"
- 2) Show **Test navigator** and enable your test targets
- 3) Add test cases
+ 2) Add test cases
  
+ ## You may also need this
+ 
+ 1) Show **Test Navigator** and eventually enable your test targets
+ 2) For a console application edit scheme and add your unit test target under the **Test** phase
+ 3) Make sure you share your classes within the unit test target if not yet done automatically
+
  ## Setup Code Coverage
  
  1) Edit scheme of your application
- 2) Choose **Test**
+ 2) Choose **Test** phase
  3) Enable option **Gather Coverage Data**
- 3) Run test an go to **Report navigator**
+ 3) Run test and go to **Report navigator**
  
  ## Some Code Examples
  
