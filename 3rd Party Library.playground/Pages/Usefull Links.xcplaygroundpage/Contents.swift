174eba1e04709e5d5adadfc2316a16f992990688
Gaetano Causio <gaetano.causio@gmail.com>
2017-05-19 11:13:24 +0200

    Usefull links section added

---
+++ 3rd Party Library.playground/Pages/Usefull Links.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*: (C) Gaetano Causio 2017
+ 
+ # Usefull Links
+ 
+ A collection of handy quick links to most of the resources that you may need during the development cycle.
+ 
+ ## AI, Machine Learning
+ 
+ [IBM Bluemix](https://bluemix.net)    \
+ [IBM Watson API Documentation](https://www.ibm.com/watson/developercloud/services-catalog.html) \
+ [IBM Watson Swift SDK](https://github.com/watson-developer-cloud/swift-sdk) 
+
+ 
+ ## Graphics, Art, Creativity
+ 
+ [Text to ASCII Art Generator](http://patorjk.com/software/taag/) \
+ [shields.io Tag Banners for Github](https://shields.io)
+ 
+ */
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Apple Programming Quick Reference
 
-[![Shippable](https://img.shields.io/badge/platform-playground-lightgrey.svg)]()
+[![Shippable](https://img.shields.io/badge/platform-playground-blue.svg)]()
 [![Shippable](https://img.shields.io/badge/language-swift-orange.svg)]()
-[![Shippable](https://img.shields.io/badge/license-apache%202.0-blue.svg)]()
+[![Shippable](https://img.shields.io/badge/license-apache%202.0-lightgrey.svg)]()
 
 A quick reference documentation for all Apple development ecosystems.
 
