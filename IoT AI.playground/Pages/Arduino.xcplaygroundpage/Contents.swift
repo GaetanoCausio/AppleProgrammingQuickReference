53322d9ebc8bb17c071859812ce3a6760aedbf88
Gaetano Causio <gaetano.causio@gmail.com>
2017-10-25 16:04:07 +0200

    Added Arduino section

---
+++ IoT AI.playground/Pages/Arduino.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*: (C) Gaetano Causio 2017
+ 
+ # Arduino
+ 
+ ## Initial Set Up & Test
+ 
+ - Connect USB cable from the Mac to the Arduino board
+ - Start Arduino application and load a sketch via **File > Examples > 1.Basics > Blink**
+ - Select correct board via **Tools > Board** menu 
+ - Select correct serial port via **Tools > Port**
+ - Upload sketch to Arduino via **Sketch > Upload**
+ 
+ 
+ 
+ ## Handy links
+ 
+ [Arduino Download](http://www.arduino.org/downloads) \
+ [Arduino Guide](http://www.arduino.org/guide) \
+ 
+ */
